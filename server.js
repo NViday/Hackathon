ab6422977459b23d2a9f803d23373cb7d38643c6
NViday <nviday3@uw.edu>
2019-04-25 13:44:37 -0700

    "fixing heroku 2.7 - lost count"

--- server.js
+++ server.js
@@ -4,23 +4,18 @@
 
 //npm modules
 let express = require('express'),
- session = require('express-session'),
  passport = require('passport'),
- mongoose = require('mongoose'),
  cors = require('cors'),
  bodyParser = require('body-parser'),
  path = require('path'),
  fs = require('fs'),
  jwt = require('express-jwt'),
  compression = require('compression'),
- winston = require('winston'),
- winstonPapertrail = require('winston').Papertrail;
-
-
+ express_winston= require('express-winston');
 
 //custom modules 
 let config = require("./src/config");
-//let logger = require("./src/utilities/logger");
+let logger = require("./src/utilities/logger");
 
 //init app
 let app = express();
@@ -28,24 +23,21 @@ let app = express();
 //init middleware
 app.use(cors());
 app.use(compression());
-app.use(express.static("public"));
-app.use(session({secret: config.secret_code }));
+app.use('/public', express.static(path.join(__dirname, './src/routes')));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(passport.initialize());
-app.use(passport.session());
 
-// init logger (winston + papertrail)
+
+
+// init my winston middleware
+// pass it my winston logger 
 app.use(
-        expressWinston.logger({
-                transports: [
-                        new winston.transports.Papertrail({
-                                host: config.logger.host,
-                                port: config.logger.port,
-                                level: 'debug',
-                        }),
-                ],
-                meta: true,
+        express_winston.logger({
+                transports: [logger],
+                meta: false,
+                expressFormat : true,
+                colorize: true,
         }),
 );
       
@@ -59,7 +51,7 @@ app.get('/', (req, res) => {
     }
 );
     
-require('./app/routes/user_routes.js')(app);
+require('./src/routes/user_routes.js')(app);
 
 //no auth on following routes
 app.use(
@@ -97,7 +89,7 @@ require('./utils/database');
 
 // loop through all routes and dynamically require them – passing api
 fs.readdirSync(path.join(__dirname, 'routes')).map(file => {
-          require('./routes/' + file)(api);
+          require('./src/routes/' + file)(api);
   });
 
 // output the status of the api in the terminal
